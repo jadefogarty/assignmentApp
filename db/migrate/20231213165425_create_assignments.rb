eb0e58f340665722eefa71afddfca613b89d466f
jadefogarty <jadefogarty@gmail.com>
2023-12-16 04:38:02 +0000

    Rubocop - no offences

--- db/migrate/20231213165425_create_assignments.rb
+++ db/migrate/20231213165425_create_assignments.rb
@@ -4,13 +4,9 @@
 class CreateAssignments < ActiveRecord::Migration[7.1]
   def change
     create_table :assignments do |t|
-      t.string :name
-      t.string :description
-      t.string :module
-      t.string :lecturer
+      t.string :name, :description, :module, :lecturer, :status
       t.datetime :due_date
       t.float :weighting
-      t.string :status
       t.boolean :submitted
       t.timestamps
     end
